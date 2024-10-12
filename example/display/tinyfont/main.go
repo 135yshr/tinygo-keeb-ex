f4509e589df98ca30a7d8df9814ea7fbab8a8da1
135yshr <isago@oreha.dev>
2024-10-12 17:23:13 +0900

    :sparkles: Add sample of tiny font

---
+++ example/display/tinyfont/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"time"
+
+	"github.com/135yshr/tinygo-keeb-ex/pkg/color"
+	"github.com/135yshr/tinygo-keeb-ex/pkg/pin"
+
+	"tinygo.org/x/tinyfont"
+	"tinygo.org/x/tinyfont/freemono"
+	"tinygo.org/x/tinyfont/gophers"
+)
+
+func main() {
+	display := pin.NewDisplay()
+	display.Clear()
+	time.Sleep(50 * time.Millisecond)
+
+	tinyfont.WriteLine(display.Device(), &freemono.Bold9pt7b, 5, 10, "hello", color.White)
+	tinyfont.WriteLine(display.Device(), &gophers.Regular32pt, 5, 50, "ABCEF", color.White)
+	display.Display()
+}
