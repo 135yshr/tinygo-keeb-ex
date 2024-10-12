a553b87227b033dddfe043d58c618dcbf87e4e72
135yshr <isago@oreha.dev>
2024-10-12 17:44:17 +0900

    :sparkles: Add a parameter to rotate the display

--- example/display/rotated/main.go
+++ example/display/rotated/main.go
@@ -16,10 +16,14 @@ func main() {
 	display.Clear()
 	time.Sleep(50 * time.Millisecond)
 
-	rotDisplay := display.Rotated()
+	for _, r := range []pin.Rotate{pin.Rotate90, pin.Rotate270} {
+		display.Clear()
+		rotDisplay := display.Rotated(r)
 
-	tinyfont.WriteLine(rotDisplay, &freemono.Bold9pt7b, 5, 10, "hello", color.White)
-	tinyfont.WriteLine(rotDisplay, &gophers.Regular58pt, 10, 70, "B", color.White)
-	tinyfont.WriteLine(rotDisplay, &gophers.Regular58pt, 10, 110, "H", color.White)
-	display.Display()
+		tinyfont.WriteLine(rotDisplay, &freemono.Bold9pt7b, 5, 10, "hello", color.White)
+		tinyfont.WriteLine(rotDisplay, &gophers.Regular58pt, 10, 70, "B", color.White)
+		tinyfont.WriteLine(rotDisplay, &gophers.Regular58pt, 10, 110, "H", color.White)
+		display.Display()
+		time.Sleep(5 * time.Second)
+	}
 }
--- pkg/pin/display.go
+++ pkg/pin/display.go
@@ -8,6 +8,15 @@ import (
 	"tinygo.org/x/drivers/ssd1306"
 )
 
+const (
+	Rotate0 Rotate = iota
+	Rotate90
+	Rotate180
+	Rotate270
+)
+
+type Rotate int
+
 type Display struct {
 	disp ssd1306.Device
 }
@@ -35,8 +44,8 @@ func (d *Display) Device() *ssd1306.Device {
 	return &d.disp
 }
 
-func (d *Display) Rotated() *RotatedDisplay {
-	return &RotatedDisplay{&d.disp}
+func (d *Display) Rotated(r Rotate) *RotatedDisplay {
+	return &RotatedDisplay{&d.disp, r}
 }
 
 func (d *Display) SetPixel(x, y int16, c color.RGBA) {
@@ -53,6 +62,7 @@ func (d *Display) Clear() {
 
 type RotatedDisplay struct {
 	drivers.Displayer
+	rotate Rotate
 }
 
 func (d *RotatedDisplay) Size() (x, y int16) {
@@ -60,6 +70,15 @@ func (d *RotatedDisplay) Size() (x, y int16) {
 }
 
 func (d *RotatedDisplay) SetPixel(x, y int16, c color.RGBA) {
-	_, sy := d.Displayer.Size()
-	d.Displayer.SetPixel(y, sy-x, c)
+	sx, sy := d.Displayer.Size()
+	switch d.rotate {
+	case Rotate0:
+		d.Displayer.SetPixel(x, y, c)
+	case Rotate90:
+		d.Displayer.SetPixel(y, sy-x, c)
+	case Rotate180:
+		d.Displayer.SetPixel(sx-y, sy-x, c)
+	case Rotate270:
+		d.Displayer.SetPixel(sx-y, x, c)
+	}
 }
