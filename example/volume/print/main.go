d6ffcc24887fd652df5926cf9cdc681d49541b4a
135yshr <isago@oreha.dev>
2024-10-12 16:24:16 +0900

    :sparkles: Add volume func

---
+++ example/volume/print/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/135yshr/tinygo-keeb-ex/pkg/pin"
+
+func main() {
+	vol := pin.NewVolume()
+
+	for oldValue := 0; ; {
+		if newValue := vol.Value(); newValue != oldValue {
+			println("value: ", newValue)
+			oldValue = newValue
+		}
+	}
+}
